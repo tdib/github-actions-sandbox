774b4f44aab9dbb3e60d42fc57a13ba526c1ec0d
Dib <79500311+tdib@users.noreply.github.com>
2024-11-12 20:16:57 +1100

    Add more logic to index.js

--- .github/actions/commit-message-validator/index.js
+++ .github/actions/commit-message-validator/index.js
@@ -1,1 +1,12 @@
-console.log("Test")
+const core = require("@actions/core")
+
+try {
+  // Get inputs
+  const pattern = new RegExp(core.getInput("pattern"))
+  console.log("Pattern", pattern)
+  const errorMessage = core.getInput("error-message")
+  console.log("Error", errorMessage)
+} catch (error) {
+  core.setFailed(error.message)
+  console.log("Something failed", error)
+}
