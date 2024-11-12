0cea38c7bc2a0010c6f4f2e25a226831194180f3
Dib <79500311+tdib@users.noreply.github.com>
2024-11-12 21:20:06 +1100

    Update formatting for invalid commit messages

--- .github/actions/commit-message-validator/index.js
+++ .github/actions/commit-message-validator/index.js
@@ -41,8 +41,8 @@ async function validateCommitMessages() {
       core.setFailed(
         [
           errorMessage,
-          "Invalid commit messages detected",
-          invalidMessages.map((msg) => `"${msg}"`).join("\n")
+          "Invalid commit messages detected:",
+          invalidMessages.map((msg) => `\t"${msg}"`).join("\n")
         ].join("\n")
       )
     } else {
