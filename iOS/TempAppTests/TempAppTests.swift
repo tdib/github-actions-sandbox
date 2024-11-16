7c6950f09d8f9eb1c758bafe335a746eef0da069
Dib <79500311+tdib@users.noreply.github.com>
2024-11-16 18:45:21 +1100

    Add dummy iOS project

---
+++ iOS/TempAppTests/TempAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  TempAppTests.swift
+//  TempAppTests
+//
+//  Created by Thomas Dib on 16/11/2024.
+//
+
+import Testing
+@testable import TempApp
+
+struct TempAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
