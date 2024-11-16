df3349d37fadfe98713fc5f19c0b4d684a2905dc
Dib <79500311+tdib@users.noreply.github.com>
2024-11-16 18:45:58 +1100

    Add dummy iOS project (#7)

---
+++ iOS/TempApp/TempAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TempAppApp.swift
+//  TempApp
+//
+//  Created by Thomas Dib on 16/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct TempAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
