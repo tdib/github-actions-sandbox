70b1a54102b45f41289bf7ef8ead22ba0529ce15
Dib <79500311+tdib@users.noreply.github.com>
2025-01-18 15:57:24 +1100

    Detect native file changes (#13)

--- Android/app/src/main/java/xyz/tdib/tempapp/MainActivity.kt
+++ Android/app/src/main/java/xyz/tdib/tempapp/MainActivity.kt
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     TempAppTheme {
-        Greeting("Android")
+        Greeting("Android Team!")
     }
 }
