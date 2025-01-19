5875512115c74b08a93c8c51959b6e6d749ce8bb
Dib <79500311+tdib@users.noreply.github.com>
2025-01-19 11:33:55 +1100

    Add Gradle property injection (#16)

--- Android/build.gradle.kts
+++ Android/build.gradle.kts
@@ -4,3 +4,11 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
 }
+
+val requiredProperties = listOf("required_value")
+
+requiredProperties.forEach { property ->
+    if (!project.hasProperty(property)) {
+        throw GradleException("Required property '$property' is not defined in your gradle.properties file.")
+    }
+}
